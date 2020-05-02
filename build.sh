376424307cceb616c4d59e52784ce355ae657082
Richard Foster <richard@rpf.me>
2020-05-02 13:13:09 +0100

    Move to putting output in public

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-mkdir -p dist
-browserify client/index.js -o dist/index.js
-browserify client/service-worker.js -o dist/service-worker.js
-cp -R public/ dist
+mkdir -p public
+browserify client/index.js -o public/index.js
+browserify client/service-worker.js -o public/service-worker.js
+cp -R static/ public
 echo "> built"
